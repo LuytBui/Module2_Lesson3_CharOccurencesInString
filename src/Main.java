c441dd0d6c4312de644aa8ce2c8f56bba4cc9bda
LuytBui <luytbq43@gmail.com>
2022-01-06 10:18:31 +0700

    done

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+//    đếm số lần xuất hiện của một ký tự được nhập vào từ bàn phím cho một chuỗi cho trước.
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập vào một chuỗi: ");
+        String s = sc.nextLine();
+        System.out.print("Nhập vào ký tự cần đếm: ");
+        char ch = sc.next().charAt(0);
+
+        int count = occurences(s, ch);
+        System.out.printf("Số lần xuất hiện của '%c' trong \"%s\" là %d", ch, s, count);
+    }
+
+    public static int occurences(String s, char ch) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ch) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
